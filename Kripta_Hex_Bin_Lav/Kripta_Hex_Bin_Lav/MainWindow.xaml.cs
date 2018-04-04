e9628dd7f845481ad20c530fa7624acbd5a32a9e
Ilya Lavrov <ilyalavrov97@gmail.com>
2018-04-04 21:16:07 +0300

    Avalanche effect completed

---
+++ Kripta_Hex_Bin_Lav/Kripta_Hex_Bin_Lav/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace Kripta_Hex_Bin_Lav
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string HexToBinary(string hexstring)
+        {
+            string binarystring = String.Join(String.Empty,
+                hexstring.Select(c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')));
+
+            return binarystring;
+        }
+
+        private double CompareStrings(string str1, string str2)
+        {
+            double counter = 0;
+
+            for (int i = 0; i < Math.Min(str1.Length, str2.Length); i++)
+            {
+                if (str1[i] != str2[i])
+                    counter++;
+            }
+
+            return counter / (double)Math.Min(str1.Length, str2.Length);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string bin1 = HexToBinary(InitialTB.Text);
+            string bin2 = HexToBinary(ChangedTB.Text);
+
+            double result = CompareStrings(bin1, bin2);
+
+            AnswerTB.Text = result.ToString();
+        }
+    }
+}
